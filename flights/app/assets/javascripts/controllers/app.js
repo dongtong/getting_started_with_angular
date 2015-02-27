e632da04bd8b43936ff361d98509e1e8d91f56ce
dongtong <jinhu.tung@gmail.com>
2015-02-27 16:41:40 +0800

    filter

--- flights/app/assets/javascripts/controllers/app.js
+++ flights/app/assets/javascripts/controllers/app.js
@@ -1,5 +1,17 @@
 var flightApp = angular.module('flightApp', []);
-flightApp.controller('flightApp.FirstController', function($scope){
+flightApp.controller('flightApp.FirstController', ['$scope', '$interval', function($scope, $interval){
+	//The reason is that two-way binding in angular uses dirty checking. 
+	//This is a good article to read about angular's dirty checking. 
+	//$scope.$apply() kicks off a $digest cycle. 
+	//This will apply the binding. $timeout handles the $apply for you so it is the recommended service to use when using timeouts.
+	$scope.today = Date.now();
+	// setInterval(function(){
+	// 	$scope.today = Date.now();
+	// 	$scope.$apply(); 
+	// }, 1000);
+  $interval(function(){
+    $scope.today = Date.now();
+  },1000);
   $scope.airports = [{
       "code": "PDX",
       "name": "Portland International Airport",
@@ -15,6 +27,10 @@ flightApp.controller('flightApp.FirstController', function($scope){
       "name": "Kansas City International Airport",
       "city": "Kansas City",
       "destinations": ["LAX","DFW"]
-    }];
+  }];
 
-});
+  $scope.isCapitalized = function(str){
+		return str[0] == str[0].toUpperCase();
+  }
+
+}]);
